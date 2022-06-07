4cac2bb9bbe2734d49a36a6d5f38c78e66e9d032
Mark Mavis <mmavis@uw.edu>
2022-06-07 14:19:34 -0700

    update section 5

--- Section03/Section 5/103_replacing_slices.py
+++ Section03/Section 5/103_replacing_slices.py
@@ -1,5 +1,8 @@
 
 # Replacing an item in a list
+import re
+
+
 s = ["a", "f", "c", "d"]
 s[1] = "b"
 
@@ -24,5 +27,41 @@ computer_parts = ["computer",
                   "mouse",
                   "mouse mat"]
 
-computer_parts[3:] = "trackball"
+
+computer_parts[3:] = "trackball"    # Enters "trackball" chars as individual iteams
+                                    # The "trackball" string is an iterable and views them
+                                    #   as individual items that are strings of length 1
 print(computer_parts)
+del computer_parts[3:]
+print(computer_parts)
+computer_parts[3:] = ["trackball"]
+print(computer_parts)
+
+min_val = 100
+max_val = 200
+stop = None
+
+data = [4, 5, 104, 105, 110, 120, 130, 130, 150,
+        160, 170, 183, 185, 187, 188, 191, 350, 360, 400, 600, 750, 1000]
+
+# Enumerating the list forward
+for index, val in enumerate(data):
+    # print(f"index: {index} Val: {val}")
+    if val >= min_val:
+        stop = index
+        break
+if stop != None:
+    del data[:stop]
+
+print(data)     # Print List
+
+# Enumerating the list in reverse
+for index, val in enumerate(reversed(data)):
+    # print(f"index: {index} Val: {val}")
+    if val <= max_val:
+        stop = index
+        break
+if stop != None:
+    del data[-stop:]
+
+print(data)     # Print List
