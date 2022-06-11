f258a72966db712116a51a95c59e92c24448a346
Mark Mavis <mmavis@uw.edu>
2022-06-10 21:56:34 -0700

    Add Nested Tuples Exercise

--- Masterclass/Section 5/121_tuples.py
+++ Masterclass/Section 5/121_tuples.py
@@ -40,3 +40,51 @@ for title, artist, release_year in albums:
 	print(f"Artist: {artist} Album Title: {title} Release Year: {release_year}")
 
 # Nested Tuples
+new_albums = [
+    ("Welcome to my Nightmare", "Alice Cooper", 1975,
+     [
+         (1, "Welcome to my Nightmare"),
+         (2, "Devil's Food"),
+         (3, "The Black Widow"),
+         (4, "Some Folks"),
+         (5, "Only Women Bleed"),
+     ]
+     ),
+    ("Bad Company", "Bad Company", 1974,
+     [
+         (1, "Can't Get Enough"),
+         (2, "Rock Steady"),
+         (3, "Ready for Love"),
+         (4, "Don't Let Me Down"),
+         (5, "Bad Company"),
+         (6, "The Way I Choose"),
+         (7, "Movin' On"),
+         (8, "Seagull"),
+     ]
+     ),
+    ("Nightflight", "Budgie", 1981,
+     [
+         (1, "I Turned to Stone"),
+         (2, "Keeping a Rendezvous"),
+         (3, "Reaper of the Glory"),
+         (4, "She Used Me Up"),
+     ]
+     ),
+    ("More Mayhem", "Imelda May", 2011,
+     [
+         (1, "Pulling the Rug"),
+         (2, "Psycho"),
+         (3, "Mayhem"),
+         (4, "Kentish Town Waltz"),
+     ]
+     ),
+]
+
+for album, artist, release_year, songs in new_albums:
+    for track, song_title in songs:
+        print(f"{artist} - {album} - {track} - {song_title}")
+
+print(new_albums[3])
+print(new_albums[3][3])
+print(new_albums[3][3][2])
+print(new_albums[3][3][2][1])
