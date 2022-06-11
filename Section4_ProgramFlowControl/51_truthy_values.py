40f69060032340458be09108e64f3704a0e7995d
Mark Mavis <mmavis@uw.edu>
2022-06-10 23:37:48 -0700

    Add Sections 3 and 4

---
+++ Section4_ProgramFlowControl/51_truthy_values.py
@@ -0,0 +1,7 @@
+
+name = input("Enter your name")
+
+if name:
+    print("Nice to meet you {0}".format(name))
+else:
+    print("Do you not have a name?")
---
+++ Section4_ProgramFlowControl/53_challenge.py
@@ -0,0 +1,10 @@
+
+name = str(input("What is your name: "))
+age = int(input("What is your age: "))
+
+if 18 <= age <= 31:
+    print("Welcome to the holiday")
+else:
+    print("Sorry, you aren't the correct age")
+
+
