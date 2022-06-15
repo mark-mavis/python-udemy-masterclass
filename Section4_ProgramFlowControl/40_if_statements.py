19aa4a39dd2459e292dae678fefadbd1a29cd8a0
Mark Mavis <mmavis@uw.edu>
2022-06-15 00:42:12 -0700

    Delete Section4_ProgramFlowControl directory

--- Section4_ProgramFlowControl/40_if_statements.py
+++
@@ -1,7 +0,0 @@
-name = input("Please enter your name: ")
-age = int(input("How old are you, {0}?".format(name)))
-
-if age >= 18:
-    print("You are old enough to vote!")
-else:
-    print("Please come back in {} years".format(18-age))
